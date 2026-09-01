94c177e5dcbefb159514bd90f2352fb2c3da175a
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-01 12:56:41 +0300

    Add constructors and Value property to clsString

--- clsString.h
+++ clsString.h
@@ -1,1 +1,35 @@
 #pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class clsString
+{
+private:
+	string _Value;
+
+public:
+
+    clsString()
+    {
+        _Value = "";
+    }
+
+    clsString(string Value)
+    {
+        _Value = Value;
+    }
+
+    void SetValue(string Value) {
+        _Value = Value;
+    }
+
+    string GetValue() {
+        return _Value;
+    }
+
+    __declspec(property(get = GetValue, put = SetValue)) string Value;
+
+
+};
