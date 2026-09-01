b8b1320e88aea16e8ad5d921f013189db91b8821
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-01 15:43:30 +0300

    feat: add RandomNumber and GetRandomCharacter methods to clsUtil

--- clsUtil.h
+++ clsUtil.h
@@ -19,5 +19,53 @@ public:
         srand((unsigned)time(NULL));
     }
 
+    static  int RandomNumber(int From, int To)
+    {
+        //Function to generate a random number
+        int randNum = rand() % (To - From + 1) + From;
+        return randNum;
+    }
+
+    static char GetRandomCharacter(enCharType CharType)
+    {
+
+        //updated this method to accept mixchars
+        if (CharType == MixChars)
+        {
+            //Capital/Samll/Digits only
+            CharType = (enCharType)RandomNumber(1, 3);
+
+        }
+
+        switch (CharType)
+        {
+
+        case enCharType::SamallLetter:
+        {
+            return char(RandomNumber(97, 122));
+            break;
+        }
+        case enCharType::CapitalLetter:
+        {
+            return char(RandomNumber(65, 90));
+            break;
+        }
+        case enCharType::SpecialCharacter:
+        {
+            return char(RandomNumber(33, 47));
+            break;
+        }
+        case enCharType::Digit:
+        {
+            return char(RandomNumber(48, 57));
+            break;
+        }
+    defualt:
+        {
+            return char(RandomNumber(65, 90));
+            break;
+        }
+        }
+    }
 
 };
