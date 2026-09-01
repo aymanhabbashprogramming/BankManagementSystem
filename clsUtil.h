3032b231940b26ad407b23714bdfdceabdec1fea
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-01 15:40:00 +0300

    feat: add enCharType enum and Srand method to clsUtil

--- clsUtil.h
+++ clsUtil.h
@@ -1,1 +1,23 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include "clsDate.h"
+
+using namespace std;
+
+class clsUtil
+{
+public:
+    enum enCharType {
+        SamallLetter = 1, CapitalLetter = 2,
+        Digit = 3, MixChars = 4, SpecialCharacter = 5
+    };
+
+    static void  Srand()
+    {
+        //Seeds the random number generator in C++, called only once
+        srand((unsigned)time(NULL));
+    }
+
+
+};
