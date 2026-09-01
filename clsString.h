3d704592ed0bb0f6b9839e786290c119b7929ad8
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-01 13:00:47 +0300

    Add string manipulation methods to clsString

--- clsString.h
+++ clsString.h
@@ -31,5 +31,70 @@ public:
 
     __declspec(property(get = GetValue, put = SetValue)) string Value;
 
+    static short Length(string S1)
+    {
+        return S1.length();
+    };
+
+    short Length()
+    {
+        return _Value.length();
+    };
+
+    static short CountWords(string S1)
+    {
+
+        string delim = " "; // delimiter  
+        short Counter = 0;
+        short pos = 0;
+        string sWord; // define a string variable  
+
+        // use find() function to get the position of the delimiters  
+        while ((pos = S1.find(delim)) != std::string::npos)
+        {
+            sWord = S1.substr(0, pos); // store the word   
+            if (sWord != "")
+            {
+                Counter++;
+            }
+
+            //erase() until positon and move to next word.
+            S1.erase(0, pos + delim.length());
+        }
+
+        if (S1 != "")
+        {
+            Counter++; // it counts the last word of the string.
+        }
+
+        return Counter;
+
+    }
+
+    short CountWords()
+    {
+        return CountWords(_Value);
+    };
+
+    static string  UpperFirstLetterOfEachWord(string S1)
+    {
+
+        bool isFirstLetter = true;
+
+        for (short i = 0; i < S1.length(); i++)
+        {
+
+            if (S1[i] != ' ' && isFirstLetter)
+            {
+                S1[i] = toupper(S1[i]);
+
+            }
+
+            isFirstLetter = (S1[i] == ' ' ? true : false);
+
+        }
+
+        return S1;
+    }
 
 };
