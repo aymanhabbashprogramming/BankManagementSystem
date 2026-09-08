099e33206f28517f83c2d9d243ac5c522a5e363e
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-08 19:46:46 +0300

    feat: implement initial structure for clsFindClientScreen

--- clsFindClientScreen.h
+++ clsFindClientScreen.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <iostream>
+#include "clsScreen.h"
+#include "clsPerson.h"
+#include "clsBankClient.h"
+#include "clsInputValidate.h"
+
+class clsFindClientScreen : protected clsScreen
+{
+};
