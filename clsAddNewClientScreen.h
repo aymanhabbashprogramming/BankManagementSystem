b50bfae697a96b1db47606cf648bef0016bcce6b
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-08 18:09:10 +0300

    feat: add clsAddNewClientScreen inherited from clsScreen

---
+++ clsAddNewClientScreen.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include "clsScreen.h"
+#include "clsBankClient.h"
+#include "clsInputValidate.h"
+#include <iomanip>
+
+class clsAddNewClientScreen : protected clsScreen
+{
+
+};
