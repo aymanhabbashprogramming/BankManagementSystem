7b2c784cb9d9c5c18deb1ad8e3ab54251efe6271
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-08 22:51:15 +0300

    feat: call ShowDepositScreen in clsTransactionsScreen

--- clsTransactionsScreen.h
+++ clsTransactionsScreen.h
@@ -3,6 +3,7 @@
 #include "clsScreen.h"
 #include "clsInputValidate.h"
 #include <iomanip>
+#include "clsDepositScreen.h"
 
 using namespace std;
 
@@ -23,7 +24,7 @@ private:
 
     static void _ShowDepositScreen()
     {
-        cout << "\n Deposit Screen will be here.\n";
+        clsDepositScreen::ShowDepositScreen();
     }
 
     static void _ShowWithdrawScreen()
