0d4846b3ef0f90aa5cb57054f8edadd754e998d9
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-09 18:34:15 +0300

    feat: integrate manage users menu option into main screen

--- clsMainScreen.h
+++ clsMainScreen.h
@@ -9,6 +9,7 @@
 #include "clsUpdateClientScreen.h"
 #include "clsFindClientScreen.h"
 #include "clsTransactionsScreen.h"
+#include "clsManageUsersScreen.h"
 using namespace std;
 
 class clsMainScreen :protected clsScreen
@@ -69,8 +70,7 @@ private:
 
     static void _ShowManageUsersMenue()
     {
-        cout << "\nUsers Menue Will be here...\n";
-
+        clsManageUsersScreen::ShowManageUsersMenue();
     }
 
     static void _ShowEndScreen()
