929b5aebfdc4e93c4a04be8a3fd961f0b0c03554
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-09 15:58:15 +0300

    feat: call ShowTotalBalancesScreen in clsTransactionsScreen

--- clsTransactionsScreen.h
+++ clsTransactionsScreen.h
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include "clsDepositScreen.h"
 #include "clsWithdrawScreen.h"
+#include "clsTotalBalancesScreen.h"
 
 using namespace std;
 
@@ -35,7 +36,7 @@ private:
 
     static void _ShowTotalBalancesScreen()
     {
-        cout << "\n Balances Screen will be here.\n";
+        clsTotalBalancesScreen::ShowTotalBalances();
     }
 
     static void _GoBackToTransactionsMenue()
