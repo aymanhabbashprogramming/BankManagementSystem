a8f3f79397f1f9df540e89d0cd90363456f88c05
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-11 17:50:08 +0300

    feat: add clsAddNewUserScreen skeleton inherited from clsScreen

---
+++ clsAddNewUserScreen.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include "clsScreen.h"
+#include "clsUser.h"
+#include "clsInputValidate.h"
+#include <iomanip>
+
+class clsAddNewUserScreen : protected clsScreen
+{
+
+};
