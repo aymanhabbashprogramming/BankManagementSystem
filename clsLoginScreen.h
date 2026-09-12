b542bcfcad461a0055e7dc591b0840bbb4c255a0
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-12 10:47:08 +0300

    feat: add clsLoginScreen skeleton for authentication management

---
+++ clsLoginScreen.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include "clsScreen.h"
+#include "clsUser.h"
+#include <iomanip>
+#include "clsMainScreen.h"
+#include "Global.h"
+
+class clsLoginScreen :protected clsScreen
+{
+
+};
