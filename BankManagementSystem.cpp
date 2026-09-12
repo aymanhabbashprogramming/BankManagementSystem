f0a595e773054ff6f5f08ed102142a304315077d
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-12 11:41:10 +0300

    refactor: update main to test login and logout process

--- BankManagementSystem.cpp
+++ BankManagementSystem.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include "clsMainScreen.h"
 #include "clsLoginScreen.h"
 
 int main()
 
 {
-    //clsMainScreen::ShowMainMenue();
     clsLoginScreen::ShowLoginScreen();
     system("pause");
     return 0;
