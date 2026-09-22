ea3c9ff4b958713716c3d66155cde7de24d70572
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-09-22 12:29:46 +0300

    feat(ui): connect Find Currency screen option to Currency Exchange menu

--- clsCurrencyExchangeMainScreen.h
+++ clsCurrencyExchangeMainScreen.h
@@ -4,6 +4,7 @@
 #include "clsInputValidate.h"
 #include <iomanip>
 #include "clsCurrenciesListScreen.h"
+#include "clsFindCurrencyScreen.h"
 
 using namespace std;
 
@@ -36,8 +37,7 @@ private:
 
     static void _ShowFindCurrencyScreen()
     {
-        cout << "\nFind Currency Screen Will Be Here.\n";
-
+        clsFindCurrencyScreen::ShowFindCurrencyScreen();
     }
 
     static void _ShowUpdateCurrencyRateScreen()
